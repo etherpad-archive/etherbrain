2a8d7ef8799044f0cd1dd50123ea27cbed74f8d4
Tim Head <betatim@gmail.com>
2016-02-11 17:05:35 +0100

    First commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+
+import requests
+from github3 import login
+
+from flask import (
+    Response,
+    Flask,
+    g,
+    request
+)
+
+GH_TOKEN = os.getenv("TOKEN")
+
+
+app = Flask(__name__)
+app.debug = True
+
+
+@app.route('/moz/<path:path>/')
+def moz_pad(path):
+    ether_path = "https://public.etherpad-mozilla.org/p/{}".format(path)
+    req = requests.get(ether_path + "/export/txt")
+    
+    gh = login('etherbrain', token=GH_TOKEN)
+    r = gh.repository('etherpad-archive', 'etherpad-archive.github.io')
+    contents = r.contents(path='moz')
+
+    if path in contents:
+        # update the file
+        return Response(
+            "Check out: http://etherpad-archive.github.io/moz/{}".format(path)
+        )
+    else:
+        # create it for the first time
+        r.create_file("moz/" + path,
+                      'etherpad from {}'.format(ether_path),
+                      content=req.content)
+    
+    return Response(
+        "Check out: http://etherpad-archive.github.io/moz/{}".format(path)
+    )
+
+@app.route('/')
+def index():
+    return Response("Hello I am the etherpad brain")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
